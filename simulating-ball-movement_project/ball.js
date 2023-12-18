003c032adfdc609dabdbeaf817402db56222a920
Blair Laird <146311683+blairlaird@users.noreply.github.com>
2023-12-18 00:27:23 -0700

    Add files via upload

--- simulating-ball-movement_project/ball.js
+++ simulating-ball-movement_project/ball.js
@@ -1,40 +1,76 @@
-//Set global variable that would contain the position, velocity and the html element "ball"
-var positionX = 0;
-var positionY = 0;
-var velocity = 100;
-var reverse = false;
-var ball = document.getElementById("ball");
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Bouncing Ball with Edge Detection</title>
+  <style>
+    #ball {
+      z-index: 5;
+      position: absolute;
+      left: 0px;
+      top: 0px;
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      background: black;
+    }
+  </style>
+</head>
+<body>
+  <div id="ball"></div>
 
-//write a function that can change the position of the html element "ball"
-function moveBall() {
-  // two x-axis coordinates
-  var Xmin = 0;
-  var Xmax = 300;
+  <script>
+    // Set global variables for the ball element, velocity, position, and edge boundaries
+    var ball = document.getElementById("ball");
+    var velocity = 10; // Initial velocity
+    var positionX = 0; // Initial x-axis position
+    var positionY = 0; // Initial y-axis position
+    var reverseX = false; // Flag for reversing x-axis ball direction
+    var reverseY = false; // Flag for reversing y-axis ball direction
+    var Xmin = 0; // Left edge boundary
+    var Xmax = window.innerWidth - ball.offsetWidth; // Right edge boundary
+    var Ymin = 0; // Top edge boundary
+    var Ymax = window.innerHeight - ball.offsetHeight; // Bottom edge boundary
 
-  // two y-axis coordinates
-  var Ymin = 0;
-  var Ymax = 300;
+    // Function to move the ball and reverse its direction when it reaches the edge boundaries
+    function moveBall() {
+      // Update the position of the ball on the x-axis based on the value of reverseX
+      if (!reverseX) {
+        // Move the ball from left to right
+        positionX += velocity;
+      } else {
+        // Move the ball from right to left
+        positionX -= velocity;
+      }
 
-  if (reverse) {
-    positionX = positionX - velocity;
-    positionY = positionY - velocity;
-    ball.style.left = positionX + "px";
-    ball.style.top = positionY + "px";
-  } else {
-    positionX = positionX + velocity;
-    positionY = positionY + velocity;
-    ball.style.left = positionX + "px";
-    ball.style.top = positionY + "px";
-  }
+      // Update the position of the ball on the y-axis based on the value of reverseY
+      if (!reverseY) {
+        // Move the ball from top to bottom
+        positionY += velocity;
+      } else {
+        // Move the ball from bottom to top
+        positionY -= velocity;
+      }
 
-  if (
-    positionX > Xmax ||
-    positionX === Xmin ||
-    positionY > Ymax ||
-    positionY === Ymin
-  ) {
-    reverse = !reverse;
-  }
-}
+      // Check if the ball has reached the edge boundaries on the x-axis
+      if (positionX >= Xmax || positionX <= Xmin) {
+        // Reverse the direction on the x-axis by switching the value of reverseX
+        reverseX = !reverseX;
+      }
 
-setInterval(moveBall, 100);
+      // Check if the ball has reached the edge boundaries on the y-axis
+      if (positionY >= Ymax || positionY <= Ymin) {
+        // Reverse the direction on the y-axis by switching the value of reverseY
+        reverseY = !reverseY;
+      }
+
+      // Set the position of the ball
+      ball.style.left = positionX + "px";
+      ball.style.top = positionY + "px";
+    }
+
+    // Call the moveBall function every 100 milliseconds
+    setInterval(moveBall, 100);
+  </script>
+</body>
+</html>
